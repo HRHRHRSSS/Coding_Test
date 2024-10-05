0c8b709a0dc2cc831cc34b3e976907b021d7de33
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-05 15:33:32 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,6 +1,8 @@
+-- 가장 큰 물고기 10마리의 ID와 길이 출력 (LIMIT사용?)
+-- 길이 기준 내림차순, ID기준 오름차순
+-- 가장 큰 물고기 10마리 중 NULL인 경우는 x
 SELECT ID, LENGTH
 FROM FISH_INFO
-WHERE LENGTH IS NOT NULL
-ORDER BY LENGTH DESC, ID
-LIMIT 10;
+ORDER BY LENGTH DESC, ID ASC
+LIMIT 10
 
