1ba1577e0d43d00f11f086dc7f878eda74d1d59f
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-07 20:07:38 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    new_lost = set(lost) - set(reserve)
+    new_reserve = set(reserve) - set(lost)
+    
+    # 여벌 옷 가진 학생이 도난당한 학생에게 빌려주는 경우
+    for i in new_reserve:
+        if i-1 in new_lost:
+            new_lost.remove(i-1)
+        elif i+1 in new_lost:
+            new_lost.remove(i+1)
+    return n - len(new_lost)
