7cb46b88aa94283ac43696dc17454f0eb58c72c9
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-07 17:29:12 +0900

    [level 2] Title: 가장 큰 수, Time: 1503.53 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,6 @@
+from itertools import permutations
+
 def solution(numbers):
-    # 문자열으로 비교를 해야 한다
-    # 이어붙였을 때 더 큰 수를 만들 수 있는 순서 찾기 위해
-    answer = ' '
-    numbers = [str(i) for i in numbers]
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    answer = str(int(''.join(numbers)))
-    return answer
+    numbers = list(map(str,numbers))
+    numbers.sort(key=lambda x:x*3, reverse=True)
+    return str(int(''.join(numbers)))
