fb3126b204bf013375179ec072f2c253bdb813db
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-07 16:42:24 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,7 @@
 def solution(array, commands):
-    result = []
-    for p in commands:
-        i,j,k = p[0],p[1],p[2]
-        #슬라이싱 부분을 정확히 생각해야 할듯
-        list = array[i-1:j]
-        list = sorted(list)
-        result.append(list[k-1])
-    return result
+    answer = []
+    for i,q,k in commands:
+        temp = array[i-1:q]
+        temp = sorted(temp)
+        answer.append(temp[k-1])
+    return answer
