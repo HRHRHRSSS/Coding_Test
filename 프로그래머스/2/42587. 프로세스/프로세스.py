b3e6c18cad21f9850c84e2579d9f72119d08967d
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-08 16:20:48 +0900

    [level 2] Title: 프로세스, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -6,14 +6,18 @@ def solution(priorities, location):
     priorities = deque(priorities)
     
     answer = []
+    m = max(priorities)
     while q:
         temp = q.popleft()
-        if priorities[0] < max(priorities):
+        if priorities[0] < m:
             q.append(temp)
             c = priorities.popleft()
             priorities.append(c)
         else:
             answer.append(temp)
             priorities.popleft()
+            if priorities: # max는 효율bad. 그래서 pop될때만 업뎃되게
+                # priorities가 마지막에는 빈 배열되니까 존재하는 경우만
+                m = max(priorities)
     
     return answer.index(location)+1
