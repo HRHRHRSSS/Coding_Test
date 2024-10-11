2256f684b10bd1f35bd2a9b27d68fb5b9c7847f2
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-11 16:43:10 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,8 @@
+-- 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회
+-- 결과는 보호 기간이 긴 순으로 조회
+SELECT B.ANIMAL_ID, B.NAME
+FROM ANIMAL_INS A
+RIGHT JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.ANIMAL_ID IS NOT NULL -- 입양을 간 동물만
+ORDER BY DATEDIFF(B.DATETIME,A.DATETIME) DESC
+LIMIT 2
