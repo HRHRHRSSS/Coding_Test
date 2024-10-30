42cc455f0cf1aaed7bbfeaf47cc6ee00bcb46fe9
림이 <138752144+HRHRHRSSS@users.noreply.github.com>
2024-10-30 20:45:18 +0900

    [level 2] Title: 타겟 넘버, Time: 2854.44 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,16 @@
+from itertools import product
+
 def solution(numbers, target):
-    def dfs(idx, current_sum):
-        # 모든 숫자를 사용한 경우(끝까지 탐색)
-        if idx == len(numbers):
-            if current_sum == target:
-                return 1
-            else:
-                return 0
-        else:
-            add_case = dfs(idx+1, current_sum + numbers[idx])
-            subtract_case = dfs(idx+1, current_sum - numbers[idx])
-            
-            return add_case + subtract_case
-    # 첫 번째 인덱스와 합계 0에서 시작            
-    return dfs(0,0)
+    combination = product(['+','-'], repeat=len(numbers))
+    
+    answer = 0
+    for com in combination:
+        temp = 0 #연산 결과 저장
+        for num, sign in zip(numbers,com):
+            if sign == '+':
+                temp += num
+            elif sign == '-':
+                temp -= num
+        if temp == target:
+            answer += 1
+    return answer
